82559a83b7fbc1db40be65399eefcfb21ca68a7c
Artem Pavlov <Zireaeell@yandex.ru>
2022-05-28 20:23:47 +0300

    cors

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,7 +1,7 @@
 const allowedCors = [
   'http://localhost:3000',
-  'http://artempavlov.mesto.nomoredomains.work',
-  'https://artempavlov.mesto.nomoredomains.work',
+  'http://artempavlov.movies.nomoredomains.xyz',
+  'https://artempavlov.movies.nomoredomains.xyz',
   'http://127.0.0.1:5500',
 ];
 
